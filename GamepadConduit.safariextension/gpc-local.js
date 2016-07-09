dafcea5b4da9ef92f83d8d25c41b3becfe13ffc9
Arthur Langereis <puurtuur@me.com>
2016-07-09 17:27:03 +0200

    only update local gamepad state if the actual state updated

--- GamepadConduit.safariextension/gpc-local.js
+++ GamepadConduit.safariextension/gpc-local.js
@@ -3,6 +3,7 @@
 var gamepads_s = [];
 var nextGamepadIndex_s = 0;
 var commsElem = null;
+var lastGamepadDataString = "[]";
 
 
 function GamepadButtonPrototype() {
@@ -38,52 +39,59 @@ window.GamepadEvent = GamepadEventPrototype;
 navigator.getGamepads = function() {
 	if (commsElem === null) {
 		commsElem = document.querySelector('meta[name="GPCData"]');
+		commsElem.setAttribute("active", true);
 		return [];
 	}
 	else {
-		var gamepadData = JSON.parse(commsElem.getAttribute("controllers"));
+		var gamepadDataString = commsElem.getAttribute("controllers");
 
-		if (gamepadData.length === 7) {
-			if (gamepads_s.length === 0) {
-				gamepads_s.push(new GamepadPrototype());
-			}
-			var gamepad0 = gamepads_s[0];
-			var btnState = gamepadData[0];
-			var stateMask = 1;
+		if (gamepadDataString !== lastGamepadDataString) {
+			lastGamepadDataString = gamepadDataString;
+			var gamepadData = JSON.parse(gamepadDataString);
 
-			// map button bit states to GamepadButton fields
-			for (var btn = 0; btn < 17; ++btn) {
-				var button = gamepad0.buttons[btn];
-
-				// special case the triggers
-				if (btn === 6) {
-					button.value = gamepadData[5];
-					button.pressed = gamepadData[5] > 0.6;
+			if (gamepadData.length === 7) {
+				if (gamepads_s.length === 0) {
+					gamepads_s.push(new GamepadPrototype());
 				}
-				else if (btn === 7) {
-					button.value = gamepadData[6];
-					button.pressed = gamepadData[6] > 0.6;
-				}
-				else {
-					if (btnState & stateMask) {
-						button.value = 1;
-						button.pressed = true;
+				var gamepad0 = gamepads_s[0];
+				var btnState = gamepadData[0];
+				var stateMask = 1;
+
+				// map button bit states to GamepadButton fields
+				for (var btn = 0; btn < 17; ++btn) {
+					var button = gamepad0.buttons[btn];
+
+					// special case the triggers
+					if (btn === 6) {
+						button.value = gamepadData[5];
+						button.pressed = gamepadData[5] > 0.6;
+					}
+					else if (btn === 7) {
+						button.value = gamepadData[6];
+						button.pressed = gamepadData[6] > 0.6;
 					}
 					else {
-						button.value = 0;
-						button.pressed = false;
+						if (btnState & stateMask) {
+							button.value = 1;
+							button.pressed = true;
+						}
+						else {
+							button.value = 0;
+							button.pressed = false;
+						}
 					}
+
+					stateMask <<= 1;
 				}
 
-				stateMask <<= 1;
-			}
+				// copy left and right axes
+				gamepad0.axes[0] = gamepadData[1];
+				gamepad0.axes[1] = gamepadData[2];
+				gamepad0.axes[2] = gamepadData[3];
+				gamepad0.axes[3] = gamepadData[4];
 
-			// copy left and right axes
-			gamepad0.axes[0] = gamepadData[1];
-			gamepad0.axes[1] = gamepadData[2];
-			gamepad0.axes[2] = gamepadData[3];
-			gamepad0.axes[3] = gamepadData[4];
-			gamepad0.timestamp = performance.now();
+				gamepad0.timestamp = performance.now();
+			}
 		}
 
 		return gamepads_s;
