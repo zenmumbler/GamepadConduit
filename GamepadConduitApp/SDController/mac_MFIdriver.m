286eae5e5020a3e18b5e7df9b6c2d926ec616555
Arthur Langereis <puurtuur@me.com>
2016-08-12 13:26:47 +0200

    No need for an extra deadzone.

--- GamepadConduitApp/SDController/mac_MFIdriver.m
+++ GamepadConduitApp/SDController/mac_MFIdriver.m
@@ -73,7 +73,7 @@ static void MFIValueCallback(void* context, IOReturn ior, void* userData, IOHIDV
 		}
 		
 		if (button) {
-			button->pressed = elementValue > 5;
+			button->pressed = elementValue > 0;
 		}
 	}
 }
